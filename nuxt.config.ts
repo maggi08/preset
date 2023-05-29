8e634ddf0d4cb50acda30b40f1c853befa5a6104
Mako <maggi08mus@gmail.com>
2023-05-29 15:04:03 +0600

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,59 @@ export default defineNuxtConfig({
       title: 'Lokoplane - авиабилеты',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      meta: [{ name: 'description', content: 'My amazing site.' }],
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '194x194',
+          href: '/favicon-194x194.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '192x192',
+          href: '/android-chrome-192x192.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+      ],
+      meta: [
+        { name: 'description', content: 'My amazing site.' },
+        {
+          name: 'msapplication-TileColor',
+          content: '#da532c',
+        },
+        {
+          name: 'theme-color',
+          content: '#ffffff',
+        },
+      ],
     },
   },
 });
